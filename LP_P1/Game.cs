41239ec7e05bc510b3783c1293697eb81fb37a4e
Pedro Oliveira <pedrooliveira2007@hotmail.com>
2018-11-12 15:21:13 +0000

    chooseUrl and printUrl methods added

--- LP_P1/Game.cs
+++ LP_P1/Game.cs
@@ -35,33 +35,33 @@ namespace LP_P1
 
         public Game(string str)
         {
-            string[] data= str.Split(','); //splitted string
+            string[] data = str.Split(','); //splitted string
 
-            ID =                        Convert.ToInt32(data[0]);
-            name =                      data[1];
-            releaseDate =               Convert.ToDateTime(data[2]);
-            requiredAge=                Convert.ToInt32(data[3]);
-            dlcCount =                  Convert.ToInt32(data[4]);
-            metacritic =                Convert.ToInt32(data[5]);
-            movieCount =                Convert.ToInt32(data[6]);
-            recomendationCount =        Convert.ToInt32(data[7]);
-            screenshotCount =           Convert.ToInt32(data[8]);
-            owners =                    Convert.ToInt32(data[9]);
-            numberOfPlayers =           Convert.ToInt32(data[10]);
-            achievementCount =          Convert.ToInt32(data[11]);
-            controllSupport =           Convert.ToBoolean(data[12]);
-            plataformWindows =          Convert.ToBoolean(data[13]);
-            plataformLinux =            Convert.ToBoolean(data[14]);
-            plataformMac =              Convert.ToBoolean(data[15]);
-            categorySinglePlayer =      Convert.ToBoolean(data[16]);
-            categoryMultiplayer =       Convert.ToBoolean(data[17]);
-            categoryCoop =              Convert.ToBoolean(data[18]);
-            categoryIncludeLevelEditor= Convert.ToBoolean(data[19]);
-            categoryVRSupport =         Convert.ToBoolean(data[20]);
-            supportURL =                new Uri(data[21]);
-            aboutText =                 data[22];
-            headerImage =               new Uri(data[23]);
-            website.TryCreate(website,data[24], website);
+            ID = Convert.ToInt32(data[0]);
+            name = data[1];
+            releaseDate = Convert.ToDateTime(data[2]);
+            requiredAge = Convert.ToInt32(data[3]);
+            dlcCount = Convert.ToInt32(data[4]);
+            metacritic = Convert.ToInt32(data[5]);
+            movieCount = Convert.ToInt32(data[6]);
+            recomendationCount = Convert.ToInt32(data[7]);
+            screenshotCount = Convert.ToInt32(data[8]);
+            owners = Convert.ToInt32(data[9]);
+            numberOfPlayers = Convert.ToInt32(data[10]);
+            achievementCount = Convert.ToInt32(data[11]);
+            controllSupport = Convert.ToBoolean(data[12]);
+            plataformWindows = Convert.ToBoolean(data[13]);
+            plataformLinux = Convert.ToBoolean(data[14]);
+            plataformMac = Convert.ToBoolean(data[15]);
+            categorySinglePlayer = Convert.ToBoolean(data[16]);
+            categoryMultiplayer = Convert.ToBoolean(data[17]);
+            categoryCoop = Convert.ToBoolean(data[18]);
+            categoryIncludeLevelEditor = Convert.ToBoolean(data[19]);
+            categoryVRSupport = Convert.ToBoolean(data[20]);
+            supportURL = ChooseUrl(data[21]);
+            aboutText = data[22];
+            headerImage = ChooseUrl(data[23]);
+            website = ChooseUrl(data[24]);
         }
 
         public void PrintAllValues()
@@ -87,18 +87,50 @@ namespace LP_P1
             Console.WriteLine(categoryCoop);
             Console.WriteLine(categoryIncludeLevelEditor);
             Console.WriteLine(categoryVRSupport);
-            Console.WriteLine(supportURL.ToString());
+            Console.WriteLine(printUrl(supportURL));
             Console.WriteLine(aboutText);
-            Console.WriteLine(headerImage.ToString());
-            Console.WriteLine(website.ToString());
+            Console.WriteLine(printUrl(headerImage));
+            Console.WriteLine(printUrl(website));
+
+
+        }
+
+        public Uri ChooseUrl(string str)
+        {
+            if (str == "None" || str == "none")
+            {
+                Uri urlbase = null;
+                return urlbase;
+            }
+            else
+            {
+                Uri urlbase = new Uri(str);
+                return urlbase;
+            }
 
 
         }
 
 
+        public string printUrl(Uri url)
+        {
+            string s;
+            if (url == null)
+            {
+                s = "None.";
+            }
+            else
+            {
+                s = website.ToString();
+            }
+
+            return s;
+        }
+
+
+
+
 
     }
-
-
 }
 
