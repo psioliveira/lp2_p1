8840342d13de6e850981d34cfccc99d3187c8820
Pedro Oliveira <pedrooliveira2007@hotmail.com>
2018-11-24 23:54:15 +0000

    Render added

--- LP_P1/Renderer.cs
+++ LP_P1/Renderer.cs
@@ -7,27 +7,28 @@ namespace LP_P1
         public Renderer() { }
 
         //just one welcome
-        public void Welcome() {Console.Write(" Welcome, ");}
+        public void Welcome() { Console.Write(" Welcome, "); }
 
-        public void MainMenu()//print main menu 
+        public bool MainMenu()//print main menu 
         {
+            bool trySearch = false;
+
             Console.WriteLine(" select one option: ");
-            Console.WriteLine("(01) ID");
-            Console.WriteLine("(02) NAME");
-            Console.WriteLine("(03) LAUNCH DATE");
-            Console.WriteLine("(04) METACRITIC");
-            Console.WriteLine("(05) NUMBER OF DLCs");
-            Console.WriteLine("(06) RECOMENDATIONS");
-            Console.WriteLine("(07) NUMBER OF OWNERS");
-            Console.WriteLine("(08) NUMBER OF PLAYERS");
-            Console.WriteLine("(09) NUMBER OF ACHIEVEMENTS");
+            Console.WriteLine("(01) SPECIFY ORDER TYPE");
+            Console.WriteLine("(02) SEARCH FILTER");
+            Console.WriteLine("(03) START SEARCH");
+            Console.WriteLine("(00) QUIT");
+
+            int answer = Convert.ToInt32(Console.ReadLine());
+
+            return trySearch;
 
         }
 
-        
+
 
         public void SortMenu()//print sort menu 
-        {   
+        {
             Console.WriteLine("select one sort type: ");
             Console.WriteLine("(01) ID");
             Console.WriteLine("(02) NAME");
@@ -38,6 +39,11 @@ namespace LP_P1
             Console.WriteLine("(07) NUMBER OF OWNERS");
             Console.WriteLine("(08) NUMBER OF PLAYERS");
             Console.WriteLine("(09) NUMBER OF ACHIEVEMENTS");
+            Console.WriteLine("(00) BACK");
+
+
+            int answer = Convert.ToInt32(Console.ReadLine());
+
 
         }
 
@@ -58,6 +64,11 @@ namespace LP_P1
             Console.WriteLine("(12) COOPERATIVE");
             Console.WriteLine("(13) LEVEL EDITOR");
             Console.WriteLine("(14) VR READY");
+            Console.WriteLine("(00) BACK");
+
+            int answer = Convert.ToInt32(Console.ReadLine());
+
+
 
         }
 
